64283d879a840380153bf77d14fa197b152d546c
KiWi Developer <kiwifinance.bsc@gmail.com>
2020-10-22 07:58:20 +0600

    updated

--- test/CheeseSwapBEP20.spec.ts
+++ test/CheeseSwapBEP20.spec.ts
@@ -30,7 +30,7 @@ describe('CheeseSwapBEP20', () => {
   it('name, symbol, decimals, totalSupply, balanceOf, DOMAIN_SEPARATOR, PERMIT_TYPEHASH', async () => {
     const name = await token.name()
     expect(name).to.eq('CheeseSwap LPs')
-    expect(await token.symbol()).to.eq('Cake-LP')
+    expect(await token.symbol()).to.eq('Cheese-LP')
     expect(await token.decimals()).to.eq(18)
     expect(await token.totalSupply()).to.eq(TOTAL_SUPPLY)
     expect(await token.balanceOf(wallet.address)).to.eq(TOTAL_SUPPLY)
